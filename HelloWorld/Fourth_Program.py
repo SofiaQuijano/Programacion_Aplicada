ed78796b2ec1475340160e85ed63dfc372d022c8
SofiaQuijano <142341037+SofiaQuijano@users.noreply.github.com>
2023-09-05 13:28:08 -0500

    Create Fourth_Program.py

---
+++ HelloWorld/Fourth_Program.py
@@ -0,0 +1,4 @@
+#En operaciones, no varia el resultado con el uso de parentesis, siempre y cuando la operción completa este dentro de un gran parentesis 
+
+print(5 * 2 + 10 / 5)
+print((5 * 2) + (10 / 5))
