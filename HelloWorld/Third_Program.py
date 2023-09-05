6ba3294d8cfe172f6d01fffd12ce61dcbd8b6458
SofiaQuijano <142341037+SofiaQuijano@users.noreply.github.com>
2023-09-05 13:19:20 -0500

    Create Third_Program.py

---
+++ HelloWorld/Third_Program.py
@@ -0,0 +1,18 @@
+# Se puede definir la misma variable con diferentes datos
+ 
+print ("Menú")
+
+#Definir dato de desayuno
+meal = "Fruta"
+print("El desayuno es:")
+print(meal)
+
+# Definir dato del almuerzo
+meal = "Ajiaco"
+print("El almuerzo es:")
+print(meal)
+
+# Definir dato de la comida
+meal = "Cafe con pan"
+print("La cena es:")
+print(meal)
