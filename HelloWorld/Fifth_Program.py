712b323d5878fa4d89cdc057c33c4d9ce32a690e
SofiaQuijano <142341037+SofiaQuijano@users.noreply.github.com>
2023-09-07 12:47:15 -0500

    Create Fifth_Program.py

---
+++ HelloWorld/Fifth_Program.py
@@ -0,0 +1,21 @@
+# Operación matematica con variables
+
+coffee_price = 1.5
+number_of_coffees = 4
+ 
+#Imprime valor del cafe por la cantidad
+print("El valor por ",number_of_coffees, " cafes es:" ,coffee_price * number_of_coffees)
+# Prints "1.5"
+print("Cada cafe cuesta: ",coffee_price)
+# Prints "4"
+print("La cantidad de cafe pedido es:",number_of_coffees)
+ 
+# nuevo valor del cafe
+coffee_price = 2.00
+ 
+# Prints "8.0"
+print(coffee_price * number_of_coffees)
+# Prints "2.0"
+print(coffee_price)
+# Prints "4"
+print(number_of_coffees)
