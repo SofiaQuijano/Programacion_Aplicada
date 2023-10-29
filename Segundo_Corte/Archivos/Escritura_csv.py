819b22e3c4adde8fd0cd1b004408ebb023ad209d
SofiaQuijano <142341037+SofiaQuijano@users.noreply.github.com>
2023-10-28 23:02:24 -0500

    Update Escritura_csv.py

--- Segundo_Corte/Archivos/Escritura_csv.py
+++ Segundo_Corte/Archivos/Escritura_csv.py
@@ -2,8 +2,8 @@ import csv
 
 canciones = [
     ["3/21", "Amerika", "Quizas", "Señorita"],
-    ["Waiting for love", "Crazy", "Vuela", "Flaca"]
-    ["Domani Smetto", "Baracutana", "Una come te", "Como estrellas"]
+    ["Waiting for love", "Crazy", "Vuela", "Flaca"],
+    ["Domani Smetto", "Baracutana", "Una come te", "Como estrellas"],
 ]
 
 with open ("canciones.csv", "w", newline="") as file:
