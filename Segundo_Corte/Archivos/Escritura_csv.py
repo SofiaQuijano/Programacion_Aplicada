bd9b4cc8f26857d58a9b051e3cbb10e81d6aef29
SofiaQuijano <142341037+SofiaQuijano@users.noreply.github.com>
2023-10-28 23:01:10 -0500

    Update and rename Segundo_Corte/Archivos.py to Segundo_Corte/Archivos/Escritura_csv.py

---
+++ Segundo_Corte/Archivos/Escritura_csv.py
@@ -0,0 +1,11 @@
+import csv
+
+canciones = [
+    ["3/21", "Amerika", "Quizas", "Señorita"],
+    ["Waiting for love", "Crazy", "Vuela", "Flaca"]
+    ["Domani Smetto", "Baracutana", "Una come te", "Como estrellas"]
+]
+
+with open ("canciones.csv", "w", newline="") as file:
+    writer = csv.writer(file, delimiter= ",")
+    writer (canciones)
