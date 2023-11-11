9d3002bea214dd69df69172a2c06268008e2ab86
SofiaQuijano <142341037+SofiaQuijano@users.noreply.github.com>
2023-11-10 22:04:39 -0500

    Update Arrays.py

--- Numpy/Arrays.py
+++ Numpy/Arrays.py
@@ -1,7 +1,12 @@
 # Agrupar datos de mismo tipo, para poder referirnos a un tipo de dato con el mismo nombre
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5])
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
 
-print(arr)
-print(type(arr))
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
