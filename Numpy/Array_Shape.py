d4550c11b7db8cab1881a41fccd6473fa303694f
SofiaQuijano <142341037+SofiaQuijano@users.noreply.github.com>
2023-11-10 23:39:40 -0500

    Create Array_Shape.py

---
+++ Numpy/Array_Shape.py
@@ -0,0 +1,8 @@
+#Número de elementos en cada dimensión
+
+import numpy as np
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+
+print(arr.shape)
+#El ejemplo anterior devuelve (2, 4), lo que significa que la matriz tiene 2 dimensiones, donde la primera dimensión tiene 2 elementos y la segunda tiene 4.
