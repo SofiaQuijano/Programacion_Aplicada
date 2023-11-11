debab5fe266d04a161101f5747dc8df92611b79b
SofiaQuijano <142341037+SofiaQuijano@users.noreply.github.com>
2023-11-10 23:29:05 -0500

    Create Data_Types.py

---
+++ Numpy/Data_Types.py
@@ -0,0 +1,18 @@
+#Definir tipo de dato
+
+import numpy as np
+arr = np.array([1, 2, 3, 4], dtype='S')
+print(arr)
+print(arr.dtype)
+
+#i - integer
+#b - boolean
+#u - unsigned integer
+#f - float
+#c - complex float
+#m - timedelta
+#M - datetime
+#O - object
+#S - string
+#U - unicode string
+#V - fixed chunk of memory for other type ( void )
