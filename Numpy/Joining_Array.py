07dc985afb3d3f05d26ec8d851e7ed3dc0c12805
SofiaQuijano <142341037+SofiaQuijano@users.noreply.github.com>
2023-11-10 23:51:38 -0500

    Create Joining_Array.py

---
+++ Numpy/Joining_Array.py
@@ -0,0 +1,10 @@
+#Poner el contenido de dos o más matrices en una única matriz
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+
+arr2 = np.array([4, 5, 6])
+
+arr = np.concatenate((arr1, arr2))
+
+print(arr)
