4a8818c199bbbc60c91673055eebe7a898698937
SofiaQuijano <142341037+SofiaQuijano@users.noreply.github.com>
2023-11-10 23:07:49 -0500

    Update Array_Slicing.py

--- Numpy/Array_Slicing.py
+++ Numpy/Array_Slicing.py
@@ -1,3 +1,15 @@
 # tomar elementos de un indice dado a otro indice dado
 # [inicio: final] 
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[1:5])
 
+#ejemplo con negativo
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[-3:-1])
+
+#Ejemplo con step
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[1:5:2])
